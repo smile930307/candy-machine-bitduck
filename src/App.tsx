6540cb418cb43d321c040917b6a22535d7c00a42
TopSoftwareEngineer <ImamEugenaU2K@gmail.com>
2022-05-01 09:10:22 +0800

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,6 @@ import {
   ConnectionProvider,
   WalletProvider,
 } from '@solana/wallet-adapter-react';
-import { WalletDialogProvider } from '@solana/wallet-adapter-material-ui';
 
 import { ThemeProvider, createTheme } from '@material-ui/core';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
